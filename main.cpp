9ac563e63127a40fffa923a4b7d11368ba99613c
Diogo Batista <diogo.batista@handicare.com>
2022-07-10 19:58:30 +0200

    Transparency & Blending

--- main.cpp
+++ main.cpp
@@ -9,7 +9,21 @@ const unsigned int width = 1600;
 const unsigned int height = 1600;
 
 
+// Takes care of the information needed to draw the windows
+const unsigned int numWindows = 100;
+glm::vec3 positionsWin[numWindows];
+float rotationsWin[numWindows];
 
+// Takes care of drawing the windows in the right order
+unsigned int orderDraw[numWindows];
+float distanceCamera[numWindows];
+
+// Compare function
+int compare(const void* a, const void* b)
+{
+	double diff = distanceCamera[*(int*)b] - distanceCamera[*(int*)a];
+	return  (0 < diff) - (diff < 0);
+}
 
 int main()
 {
@@ -55,10 +69,12 @@ int main()
 
 	//Generates Shader object using shaders default.vert and default.frag
 	Shader shaderProgram("default.vert", "default.frag");
+	Shader grassProgram("default.vert", "grass.frag");
+	Shader winProgram("default.vert", "windows.frag");
 
-	Shader outliningProgram("outlining.vert", "outlining.frag");
 
 
+	// Take care of all light related things
 	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
 	glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
 	glm::mat4 lightModel = glm::mat4(1.0f); 
@@ -68,6 +84,9 @@ int main()
 	shaderProgram.Activate();
 	glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
 	glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+	grassProgram.Activate();
+	glUniform4f(glGetUniformLocation(grassProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+	glUniform3f(glGetUniformLocation(grassProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
 
 
 
@@ -79,20 +98,44 @@ int main()
 	glFrontFace(GL_CCW);
 
 
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
 	//Create camera object
 	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
 
 
 	// Load in the models from .gltf files
-	Model model("Resources/models/statue/scene.gltf");
+	Model ground("Resources/models/ground2/scene.gltf");
+	Model grass("Resources/models/grass/scene.gltf");
+	Model windows("Resources/models/windows/scene.gltf");
 
+
+	// Variables to create periodic event for FPS displaying
 	double prevTime = 0.0;
 	double crntTime = 0.0;
-	double timeDiff; 
+	double timeDiff;
+	// Keeps track of the amount of frames in timeDiff
 	unsigned int counter = 0;
 
-	// Disable V-Sync
-	glfwSwapInterval(0);
+
+
+	// Disable V-Sync (not advised)
+	//glfwSwapInterval(0);
+
+
+	for (unsigned int i = 0; i < numWindows; i++)
+	{
+		positionsWin[i] = glm::vec3
+		(
+			-15.0f + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (15.0f - (-15.0f)))),
+			1.0f + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (4.0f - 1.0f))),
+			-15.0f + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (15.0f - (-15.0f))))
+		);
+		rotationsWin[i] = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / 1.0f));
+		orderDraw[i] = i;
+	}
+
+
 
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
@@ -113,7 +156,8 @@ int main()
 			// This function is inside this if so it gets called a fixed
 			// number of times per second and camera speed doesnt get tied
 			// with FPS rate
-			camera.Inputs(window);
+			// Comment if vsync is enabled
+			//camera.Inputs(window);
 
 		}
 
@@ -124,19 +168,30 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
 
-
-
+		// Comment if vsync is disabled
+		camera.Inputs(window);
 		// Updates and exports the camera Matrix to the Vertex Shader
 		camera.updateMatrix(45.0f, 0.1f, 100.0f);
 
 
 		// Draw a model
-		glStencilFunc(GL_ALWAYS, 1, 0xFF);
-		glStencilMask(0xFF);
 
-		model.Draw(shaderProgram, camera);
+		ground.Draw(shaderProgram, camera);
+		glDisable(GL_CULL_FACE);
+		grass.Draw(grassProgram, camera);
+		glEnable(GL_BLEND);
+		for (unsigned int i = 0; i < numWindows; i++)
+		{
+			distanceCamera[i] = glm::length(camera.Position - positionsWin[i]);
+		}
+		qsort(orderDraw, numWindows, sizeof(unsigned int), compare);
+		for (unsigned int i = 0; i < numWindows; i++)
+		{
+			windows.Draw(winProgram, camera, positionsWin[orderDraw[i]], glm::quat(1.0f, 0.0f, rotationsWin[orderDraw[i]], 0.0f));
+		}
+		glDisable(GL_BLEND);
 
-		
+		glEnable(GL_CULL_FACE);
 
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
@@ -147,6 +202,7 @@ int main()
 
 	// Delete all the objects we've created
 	shaderProgram.Delete();
+	grassProgram.Delete();
 	// Delete window at the end of the main
 	glfwDestroyWindow(window);		
 	// Exit GLFW before leaving main
